2fcc439d9b1dda04ceb01c43636e72281ce819db
Muhin007 <35358700+Muhin007@users.noreply.github.com>
2018-04-11 22:02:50 +0600

    COLDWEB-1 Написан код запускающий сервер и класс ColdplaceServlet, который выдает одну строчку введенную аргументом

--- src/main/java/com/github/muhin007/coldplaceweb/Main.java
+++ src/main/java/com/github/muhin007/coldplaceweb/Main.java
@@ -1,9 +1,21 @@
 package com.github.muhin007.coldplaceweb;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        ColdplaceServlet coldplaceServlet = new ColdplaceServlet();
 
-        System.out.println("Сервлет показывает самый холодный город");
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(coldplaceServlet), "/coldplaceweb");
 
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        System.out.println("Сервер запущен");
+        server.join();
     }
 }
