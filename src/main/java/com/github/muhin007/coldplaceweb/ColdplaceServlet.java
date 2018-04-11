ba2dbcbb70b13832eb9244f8a8c3a939e58dfb89
Mikhail Materukhin <9226343456@mail.ru>
2018-04-11 22:31:41 +0600

    Добавили класс PageGenerator, файл index.ftl и переменную pageVariables

--- src/main/java/com/github/muhin007/coldplaceweb/ColdplaceServlet.java
+++ src/main/java/com/github/muhin007/coldplaceweb/ColdplaceServlet.java
@@ -5,14 +5,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ColdplaceServlet extends HttpServlet {
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws ServletException, IOException {
 
+
         String value = request.getParameter("key");
+
         if (value != null) {
-            response.getWriter().println(value);
+            Map<String, Object> pageVariables = new HashMap<>();
+            pageVariables.put("name", value);
+            response.getWriter().println(PageGenerator.instance().getPage("index.ftl", pageVariables));
+
         }
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
