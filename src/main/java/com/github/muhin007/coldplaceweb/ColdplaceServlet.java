fe4751520969d88b8f461afa346cb3c090fca8c1
Muhin007 <9226343456@mail.ru>
2018-04-11 14:14:17 +0500

    Написан код запускающий сервер и класс ColdplaceServlet, который выдает одну строчку введенную аргументом

---
+++ src/main/java/com/github/muhin007/coldplaceweb/ColdplaceServlet.java
@@ -0,0 +1,22 @@
+package com.github.muhin007.coldplaceweb;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ColdplaceServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+
+        String value = request.getParameter("key");
+        if (value != null)
+            response.getWriter().println(value);
+
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+    }
+}
--- src/main/java/com/github/muhin007/coldplaceweb/Main.java
+++ src/main/java/com/github/muhin007/coldplaceweb/Main.java
@@ -1,7 +1,22 @@
 package com.github.muhin007.coldplaceweb;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+
 public class Main {
-    public static void main(String[] args) {
-System.out.println("Добро пожаловать в Coldplace-web!");
-}
+    public static void main(String[] args) throws Exception {
+        ColdplaceServlet coldplaceServlet = new ColdplaceServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(coldplaceServlet), "/coldplaceweb");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        System.out.println("Сервер запущен");
+        server.join();
+    }
 }
