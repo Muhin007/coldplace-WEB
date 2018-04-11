e520cd48b58ebdc1aea5c997f5d023c240c9b049
Muhin007 <9226343456@mail.ru>
2018-04-11 16:28:08 +0500

    Ветка приведена в исходное состояние

--- src/main/java/com/github/muhin007/coldplaceweb/Main.java
+++ src/main/java/com/github/muhin007/coldplaceweb/Main.java
@@ -1,22 +1,9 @@
 package com.github.muhin007.coldplaceweb;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        ColdplaceServlet coldplaceServlet = new ColdplaceServlet();
 
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(coldplaceServlet), "/coldplaceweb");
+        System.out.println("Сервлет показывает самый холодный город");
 
-        Server server = new Server(8080);
-        server.setHandler(context);
-
-        server.start();
-        System.out.println("Сервер запущен");
-        server.join();
     }
 }
