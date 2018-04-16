a6cca8cc56c661a77bc490578fedb321d638f613
Mikhail Materukhin <9226343456@mail.ru>
2018-04-16 23:08:35 +0600

    COLDWEB-6 Добавил в файл index.ftl возможность ввода аргумента в поле. Изменил Coldplace для работы с обновленной динамической страницей.

--- src/main/java/com/github/muhin007/coldplaceweb/ColdplaceServlet.java
+++ src/main/java/com/github/muhin007/coldplaceweb/ColdplaceServlet.java
@@ -5,24 +5,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ColdplaceServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request,
-                      HttpServletResponse response) throws ServletException, IOException {
 
-
-        String value = request.getParameter("key");
-
-        if (value != null) {
-            Map<String, Object> pageVariables = new HashMap<>();
-            pageVariables.put("name", value);
-            response.getWriter().println(PageGenerator.instance().getPage("index.ftl", pageVariables));
-
-        }
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String city = request.getParameter("city");
+        city = city.toUpperCase();
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().println("<!DOCTYPE HTML>");
+        response.getWriter().println("<html><body><p>" + city + "</p></body></html>");
     }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
 }
--- templates/index.ftl
+++ templates/index.ftl
@@ -1,1 +1,16 @@
-Привет, ${name}
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Сервлет</title>
+</head>
+<body>
+
+<form action="ColdplaceServlet" method="POST">
+    <p>Введите имя пользователя: <input type="text" name="city"></p>
+    <input type="submit" value="Отправить" />
+</form>
+
+</body>
+</html>
