01b4b5c08737a65dfa3bacdffe8e91c689c0ea44
Muhin007 <9226343456@mail.ru>
2018-04-27 16:17:21 +0500

    COLDWEB-5.1 Рефакторинг сервлетов

--- src/main/java/com/github/muhin007/coldplaceweb/Main.java
+++ src/main/java/com/github/muhin007/coldplaceweb/Main.java
@@ -1,19 +1,20 @@
 package com.github.muhin007.coldplaceweb;
 
-import com.github.muhin007.coldplaceweb.Servlets.ColdplaceServlet;
 import com.github.muhin007.coldplaceweb.Servlets.ColdplaceServletButton;
+import com.github.muhin007.coldplaceweb.Servlets.ColdplaceServletSearchCity;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        ColdplaceServlet coldplaceServlet = new ColdplaceServlet();
         ColdplaceServletButton coldplaceServletButton = new ColdplaceServletButton();
+        ColdplaceServletSearchCity coldplaceServletSearchCity = new ColdplaceServletSearchCity();
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(coldplaceServlet), "/*");
         context.addServlet(new ServletHolder(coldplaceServletButton), "/button");
+        context.addServlet(new ServletHolder(coldplaceServletSearchCity), "/*");
 
         Server server = new Server(8080);
         server.setHandler(context);
---
+++ src/main/java/com/github/muhin007/coldplaceweb/Servlets/ColdplaceServletSearchCity.java
@@ -0,0 +1,50 @@
+package com.github.muhin007.coldplaceweb.Servlets;
+
+import com.github.muhin007.coldplaceweb.PageGenerator;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ColdplaceServletSearchCity extends HttpServlet{
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws IOException {
+
+        Map<String, Object> pageVariables = createPageVariablesMap(request);
+        pageVariables.put("name", "");
+
+        response.getWriter().println(PageGenerator.instance().getPage("index.html", pageVariables));
+
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws IOException {
+        Map<String, Object> pageVariables = createPageVariablesMap(request);
+
+        String name = request.getParameter("name");
+
+        response.setContentType("text/html;charset=utf-8");
+
+        if (name == null || name.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        } else {
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+        pageVariables.put("name", name == null ? "" : name);
+
+        response.getWriter().println(PageGenerator.instance().getPage("index.html", pageVariables));
+    }
+
+    private static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("parameters", request.getParameterMap().toString());
+        return pageVariables;
+    }
+}
+
