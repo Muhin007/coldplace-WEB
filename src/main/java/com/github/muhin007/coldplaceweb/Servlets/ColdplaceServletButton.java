acf72a7f766c8522b6d87a83d9a56322f515dea1
Mikhail Materukhin <9226343456@mail.ru>
2018-04-27 23:11:43 +0600

    COLDWEB-5.1 Рефакторинг

--- src/main/java/com/github/muhin007/coldplaceweb/Servlets/ColdplaceServletButton.java
+++ src/main/java/com/github/muhin007/coldplaceweb/Servlets/ColdplaceServletButton.java
@@ -10,14 +10,13 @@ public class ColdplaceServletButton extends HttpServlet {
 
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("ButtonPage.html");
+        response.sendRedirect(request.getContextPath() + "ButtonPage.html");
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) {
-        response.setContentType("text/html");
-        response.setStatus(HttpServletResponse.SC_OK);
+
     }
 }
 
