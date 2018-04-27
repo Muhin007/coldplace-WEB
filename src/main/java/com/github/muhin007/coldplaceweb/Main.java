7efbe4804b0998ab9c0f12424aac9d3728104d6d
Muhin007 <9226343456@mail.ru>
2018-04-27 14:50:56 +0500

    COLDWEB-5.1 В Main дописан сервлет ColdplaceServletButton

--- src/main/java/com/github/muhin007/coldplaceweb/Main.java
+++ src/main/java/com/github/muhin007/coldplaceweb/Main.java
@@ -7,9 +7,11 @@ import org.eclipse.jetty.servlet.ServletHolder;
 public class Main {
     public static void main(String[] args) throws Exception {
         ColdplaceServlet coldplaceServlet = new ColdplaceServlet();
+        ColdplaceServletButton coldplaceServletButton = new ColdplaceServletButton();
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(coldplaceServlet), "/*");
+        context.addServlet(new ServletHolder(coldplaceServletButton), "/button");
 
         Server server = new Server(8080);
         server.setHandler(context);
