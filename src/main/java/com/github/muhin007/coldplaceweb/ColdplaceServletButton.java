9e382ad89965016b687f0a8792212c7580804374
Muhin007 <9226343456@mail.ru>
2018-04-27 14:49:53 +0500

    COLDWEB-5.1 Добавлен сервлет ColdServletButton

---
+++ src/main/java/com/github/muhin007/coldplaceweb/ColdplaceServletButton.java
@@ -0,0 +1,48 @@
+package com.github.muhin007.coldplaceweb;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ColdplaceServletButton extends HttpServlet{
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws IOException {
+
+        Map<String, Object> pageVariables = createPageVariablesMap(request);
+        pageVariables.put("name", "");
+
+        response.getWriter().println(PageGenerator.instance().getPage("index.html", pageVariables));
+
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws IOException {
+        Map<String, Object> pageVariables = createPageVariablesMap(request);
+
+        String name = request.getParameter("name");
+
+        response.setContentType("text/html;charset=utf-8");
+
+        if (name == null || name.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        } else {
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+        pageVariables.put("name", name == null ? "" : name);
+
+        response.getWriter().println(PageGenerator.instance().getPage("index.html", pageVariables));
+    }
+
+    private static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("parameters", request.getParameterMap().toString());
+        return pageVariables;
+    }
+}
+
