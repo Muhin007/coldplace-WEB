4d0112a07a66f9bfb6d4011d9071c0e64da5ec35
Muhin007 <9226343456@mail.ru>
2018-04-28 15:06:37 +0500

    COLDWEB-5.1 Удален класс ColdplaceServlet

--- src/main/java/com/github/muhin007/coldplaceweb/DBConnection.java
+++ src/main/java/com/github/muhin007/coldplaceweb/DBConnection.java
@@ -1,28 +1,40 @@
 package com.github.muhin007.coldplaceweb;
 
 import java.sql.*;
+import java.util.Vector;
 
 public class DBConnection {
     private static final String url = "jdbc:mysql://localhost:3306/coldplace?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     private static final String user = "superuser";
     private static final String password = "coldplaceweb";
+    private Connection con = null;
 
-    String query = "select name from city";
-    {
-
-        try (Connection con = DriverManager.getConnection(url, user, password);
-             Statement stmt = con.createStatement(); ResultSet rs = stmt.executeQuery(query)) {
-
-            while (rs.next()) {
-                String name = rs.getString("name");
-                System.out.println(name);
-            }
-        } catch (SQLException sqlEx)
-
+    public Vector<Vector<Object>> getNomen(String query) {
+        Vector<Vector<Object>> retVector = new Vector<>();
         {
-            sqlEx.printStackTrace();
+
+            try (Connection con = DriverManager.getConnection(url, user, password);
+                 Statement stmt = con.createStatement(); ResultSet rs = stmt.executeQuery(query)) {
+                ResultSetMetaData rsmd = rs.getMetaData();
+                int cols = rsmd.getColumnCount();
+                while (rs.next()) {
+                    Vector<Object> newRow = new Vector<>();
+                    for (int i = 1; i <= cols; i++) {
+                        newRow.add(rs.getObject(i));
+                    }
+                    retVector.add(newRow);
+                }
+            } catch (SQLException sqlEx)
+
+            {
+                sqlEx.printStackTrace();
+            }
+            return retVector;
         }
 
     }
 }
+
+
+
 
