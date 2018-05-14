30db0dc71c8d78ee0dba6f2cb8c7d2f70384d734
Muhin007 <9226343456@mail.ru>
2018-05-14 16:43:47 +0500

    COLDWEB-5.1 Добавлен параметр для передачи нажатия "кнопки"

--- src/main/java/com/github/muhin007/coldplaceweb/Servlets/ButtonColdplaceServlet.java
+++ src/main/java/com/github/muhin007/coldplaceweb/Servlets/ButtonColdplaceServlet.java
@@ -6,21 +6,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.Map;
 
 
 public class ButtonColdplaceServlet extends HttpServlet {
 
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println(PageGenerator.instance().getPage("ButtonPage.html", new HashMap<>()));
+        Map<String, Object> pageVariables = createPageVariablesMap(request);
+        pageVariables.put("PRESS", "PRESS");
+
+        response.getWriter().println(PageGenerator.instance().getPage("ButtonPage.html", pageVariables));
+
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
     }
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) {
-        response.setContentType("text/html");
-        response.setStatus(HttpServletResponse.SC_OK);
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Map<String, Object> pageVariables = createPageVariablesMap(request);
+        response.setContentType("text/html;charset=utf-8");
+        if (request.getParameter("PRESS")!= null) {
+                response.getWriter().println("тут будет показана температура");
+                response.setContentType("text/html;charset=utf-8");
+            return;
+            }
+        else {
+            response.getWriter().println("ошибочка вышла");
+        }
+    }
+    private static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("PRESS", request.getParameterMap().toString());
+        return pageVariables;
     }
 }
