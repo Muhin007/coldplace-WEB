cc090aba7e770515302515004e9b70b344694f36
Mikhail Materukhin <9226343456@mail.ru>
2018-05-30 22:17:20 +0600

    Refactoring

--- src/main/java/com/github/muhin007/coldplaceweb/Servlets/ButtonColdplaceServlet.java
+++ src/main/java/com/github/muhin007/coldplaceweb/Servlets/ButtonColdplaceServlet.java
@@ -20,7 +20,7 @@ public class ButtonColdplaceServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Map<String, Object> pageVariables = createPageVariablesMap(request);
-        response.getWriter().println(PageGenerator.instance().getPage("ButtonPage.html", pageVariables));
+        response.getWriter().println(PageGenerator.instance().getPage("buttonPage.html", pageVariables));
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
 
@@ -29,27 +29,23 @@ public class ButtonColdplaceServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Map<String, Object> pageVariables = createPageVariablesMap(request);
 
-        List<CityWithTemp> citiesWithTemps = new ArrayList<>();
         List<City> cities = DBConnection.readDB();
-        for (City currentCity : cities) {
+        City coldestCity = cities.get(0);
+        int min = coldestCity.calculateRandomTemperature();
+
+        for (int i = 1; i < cities.size(); i++) {
+            City currentCity = cities.get(i);
             int t = currentCity.calculateRandomTemperature();
-            citiesWithTemps.add(new CityWithTemp(currentCity, t));
-        }
-        int min = citiesWithTemps.get(0).temp;
-        City coldestCity = citiesWithTemps.get(0).city;
-        for (int i = 1; i < citiesWithTemps.size(); i++) {
-            int t = citiesWithTemps.get(i).temp;
             if (t < min) {
                 min = t;
-                coldestCity = citiesWithTemps.get(i).city;
+                coldestCity = currentCity;
             }
-
         }
 
         response.setContentType("text/html;charset=utf-8");
         pageVariables.put("cityName", coldestCity.getName());
         pageVariables.put("minTemp", min);
-        response.getWriter().println(PageGenerator.instance().getPage("AnswerButtonPage.html", pageVariables));
+        response.getWriter().println(PageGenerator.instance().getPage("answerButtonPage.html", pageVariables));
     }
 
     private static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
