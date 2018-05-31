5661640dc9f5f7f6bbe6cbaec48dab7a02d80090
Muhin007 <9226343456@mail.ru>
2018-05-31 16:36:00 +0500

    Refactoring

--- src/main/java/com/github/muhin007/coldplaceweb/Servlets/ButtonColdplaceServlet.java
+++ src/main/java/com/github/muhin007/coldplaceweb/Servlets/ButtonColdplaceServlet.java
@@ -1,7 +1,6 @@
 package com.github.muhin007.coldplaceweb.Servlets;
 
 import com.github.muhin007.coldplaceweb.Data.City;
-import com.github.muhin007.coldplaceweb.Data.CityWithTemp;
 import com.github.muhin007.coldplaceweb.Data.DBConnection;
 import com.github.muhin007.coldplaceweb.PageGenerator;
 
@@ -9,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
