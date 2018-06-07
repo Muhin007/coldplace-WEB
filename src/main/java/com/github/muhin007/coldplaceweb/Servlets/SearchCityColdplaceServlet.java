13056e6541aec0fdda85a30b1b4fbf5a7331ffd5
Muhin007 <9226343456@mail.ru>
2018-06-07 12:46:36 +0500

    COLDWEB-7 Рефакторинг с применением лямбда-выражений

--- src/main/java/com/github/muhin007/coldplaceweb/Servlets/SearchCityColdplaceServlet.java
+++ src/main/java/com/github/muhin007/coldplaceweb/Servlets/SearchCityColdplaceServlet.java
@@ -1,10 +1,10 @@
 package com.github.muhin007.coldplaceweb.Servlets;
 
+import com.github.muhin007.coldplaceweb.Action;
 import com.github.muhin007.coldplaceweb.Data.City;
 import com.github.muhin007.coldplaceweb.Data.DBConnection;
 import com.github.muhin007.coldplaceweb.PageGenerator;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,51 +14,61 @@ import java.util.List;
 import java.util.Map;
 
 public class SearchCityColdplaceServlet extends HttpServlet {
+
+    static void process(HttpServletRequest request,
+                        HttpServletResponse response, Action action) throws IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=utf-8");
+
+        action.action(request, response);
+
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws IOException {
 
-        Map<String, Object> pageVariables = createPageVariablesMap(request);
-        pageVariables.put("name", "");
-
-        response.getWriter().println(PageGenerator.instance().getPage("index.html", pageVariables));
-
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
+        SearchCityColdplaceServlet.process(request, response, (HttpServletRequest req, HttpServletResponse resp) -> {
+                    Map<String, Object> pageVariables = createPageVariablesMap(req);
+                    pageVariables.put("name", "");
+                    resp.getWriter().println(PageGenerator.instance().getPage("index.html", pageVariables));
+                }
+        );
 
     }
 
+
     public void doPost(HttpServletRequest request,
                        HttpServletResponse response) throws IOException {
-        Map<String, Object> pageVariables = createPageVariablesMap(request);
+        SearchCityColdplaceServlet.process(request, response, (HttpServletRequest req, HttpServletResponse resp) -> {
+                    Map<String, Object> pageVariables = createPageVariablesMap(req);
 
-        String message = request.getParameter("name");
+                    String message = request.getParameter("name");
 
-        response.setContentType("text/html;charset=utf-8");
+                    List<City> cities = DBConnection.readDB();
+                    City foundedCity = null;
+                    for (City city : cities) {
+                        if (message.equalsIgnoreCase(city.getName())) {
+                            foundedCity = city;
 
-        List<City> cities = DBConnection.readDB();
-        City foundedCity = null;
-        for (City city : cities) {
-            if (message.equalsIgnoreCase(city.getName())) {
-                foundedCity = city;
+                            break;
+                        }
+                    }
 
-                break;
-            }
-        }
+                    if (foundedCity != null) {
 
-        if (foundedCity != null) {
+                        pageVariables.put("cityName", message);
+                        pageVariables.put("minTemp", foundedCity.calculateRandomTemperature());
+                        resp.getWriter().println(PageGenerator.instance().getPage("answerSearchCity.html",
+                                pageVariables));
+                        return;
 
-            response.setContentType("text/html;charset=utf-8");
-            pageVariables.put("cityName", message);
-            pageVariables.put("minTemp", foundedCity.calculateRandomTemperature());
-            response.getWriter().println(PageGenerator.instance().getPage("answerSearchCity.html", pageVariables));
-            return;
-
-        } else {
-            response.setContentType("text/html;charset=utf-8");
-            pageVariables.put("cityNotFound", "Города нет в списке");
-            response.getWriter().println(PageGenerator.instance().getPage("index.html", pageVariables));
-
-        }
+                    } else {
+                        pageVariables.put("cityNotFound", "Города нет в списке");
+                        resp.getWriter().println(PageGenerator.instance().getPage("index.html", pageVariables));
+                    }
+                }
+        );
 
     }
 
