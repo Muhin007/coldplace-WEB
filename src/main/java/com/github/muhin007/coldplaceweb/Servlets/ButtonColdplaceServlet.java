fc888fb2fc9959f21d7663edf30fa088c4aea2ee
Muhin007 <9226343456@mail.ru>
2018-06-07 12:46:08 +0500

    COLDWEB-7 Рефакторинг с применением лямбда-выражений

--- src/main/java/com/github/muhin007/coldplaceweb/Servlets/ButtonColdplaceServlet.java
+++ src/main/java/com/github/muhin007/coldplaceweb/Servlets/ButtonColdplaceServlet.java
@@ -1,5 +1,6 @@
 package com.github.muhin007.coldplaceweb.Servlets;
 
+import com.github.muhin007.coldplaceweb.Action;
 import com.github.muhin007.coldplaceweb.Data.City;
 import com.github.muhin007.coldplaceweb.Data.DBConnection;
 import com.github.muhin007.coldplaceweb.PageGenerator;
@@ -15,35 +16,49 @@ import java.util.Map;
 
 public class ButtonColdplaceServlet extends HttpServlet {
 
+    static void process(HttpServletRequest request,
+                        HttpServletResponse response, Action action) throws IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=utf-8");
+
+        action.action(request, response);
+
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Map<String, Object> pageVariables = createPageVariablesMap(request);
-        response.getWriter().println(PageGenerator.instance().getPage("buttonPage.html", pageVariables));
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
+        ButtonColdplaceServlet.process(request, response, (HttpServletRequest req, HttpServletResponse resp) -> {
+                    Map<String, Object> pageVariables = createPageVariablesMap(req);
+                    resp.getWriter().println(PageGenerator.instance().getPage("buttonPage.html", pageVariables));
+                }
+        );
 
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Map<String, Object> pageVariables = createPageVariablesMap(request);
+        ButtonColdplaceServlet.process(request, response, (HttpServletRequest req, HttpServletResponse resp) -> {
+                    Map<String, Object> pageVariables = createPageVariablesMap(req);
 
-        List<City> cities = DBConnection.readDB();
-        City coldestCity = cities.get(0);
-        int min = coldestCity.calculateRandomTemperature();
+                    List<City> cities = DBConnection.readDB();
+                    City coldestCity = cities.get(0);
+                    int min = coldestCity.calculateRandomTemperature();
 
-        for (int i = 1; i < cities.size(); i++) {
-            City currentCity = cities.get(i);
-            int t = currentCity.calculateRandomTemperature();
-            if (t < min) {
-                min = t;
-                coldestCity = currentCity;
-            }
-        }
+                    for (int i = 1; i < cities.size(); i++) {
+                        City currentCity = cities.get(i);
+                        int t = currentCity.calculateRandomTemperature();
+                        if (t < min) {
+                            min = t;
+                            coldestCity = currentCity;
+                        }
+                    }
 
-        response.setContentType("text/html;charset=utf-8");
-        pageVariables.put("cityName", coldestCity.getName());
-        pageVariables.put("minTemp", min);
-        response.getWriter().println(PageGenerator.instance().getPage("answerButtonPage.html", pageVariables));
+                    pageVariables.put("cityName", coldestCity.getName());
+                    pageVariables.put("minTemp", min);
+                    resp.getWriter().println(PageGenerator.instance().getPage("answerButtonPage.html",
+                            pageVariables));
+                }
+        );
+
     }
 
     private static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
