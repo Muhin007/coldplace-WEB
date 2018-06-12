53cd2c126bad07e30f5f97b01fdfa5e96770530f
Mikhail Materukhin <9226343456@mail.ru>
2018-06-12 23:03:55 +0600

    COLDWEB-7 Добавлена логика поиска url-ссылки в БД

--- src/main/java/com/github/muhin007/coldplaceweb/Servlets/URLColdplaceServlet.java
+++ src/main/java/com/github/muhin007/coldplaceweb/Servlets/URLColdplaceServlet.java
@@ -1,6 +1,8 @@
 package com.github.muhin007.coldplaceweb.Servlets;
 
 import com.github.muhin007.coldplaceweb.Action;
+import com.github.muhin007.coldplaceweb.Data.City;
+import com.github.muhin007.coldplaceweb.Data.DBConnection;
 import com.github.muhin007.coldplaceweb.PageGenerator;
 
 import javax.servlet.http.HttpServlet;
@@ -11,19 +13,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class URLColdplaceServlet extends HttpServlet {
-    class ActionImpl implements Action {
-
-        @Override
-        public void action(HttpServletRequest request, HttpServletResponse response) throws IOException {
-            Map<String, Object> pageVariables = createPageVariablesMap(request);
-            pageVariables.put("URL", "");
-            response.getWriter().println(PageGenerator.instance().getPage("URLReadPage.html", pageVariables));
-        }
-    }
-
 
     private static void process(HttpServletRequest request,
                                 HttpServletResponse response, Action action) throws IOException {
@@ -38,38 +31,54 @@ public class URLColdplaceServlet extends HttpServlet {
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws IOException {
 
-        URLColdplaceServlet.process(request, response,  (HttpServletRequest req, HttpServletResponse resp) -> {
-                Map<String, Object> pageVariables = createPageVariablesMap(req);
-                pageVariables.put("URL", "");
-                resp.getWriter().println(PageGenerator.instance().getPage("URLReadPage.html", pageVariables));
-            }
+        URLColdplaceServlet.process(request, response, (HttpServletRequest req, HttpServletResponse resp) -> {
+                    Map<String, Object> pageVariables = createPageVariablesMap(req);
+                    pageVariables.put("name", "");
+                    resp.getWriter().println(PageGenerator.instance().getPage("URLReadPage.html", pageVariables));
+                }
         );
-
 
     }
 
     public void doPost(HttpServletRequest request,
                        HttpServletResponse response) throws IOException {
         URLColdplaceServlet.process(request, response, (HttpServletRequest req, HttpServletResponse resp) -> {
-            Map<String, Object> pageVariables = createPageVariablesMap(req);
-            String message = req.getParameter("URL");
-                        URL coldplace = new URL(message);
-                    StringBuilder sb = new StringBuilder();
-                    try (BufferedReader in = new BufferedReader(
-                            new InputStreamReader(coldplace.openStream()))) {
+                    Map<String, Object> pageVariables = createPageVariablesMap(req);
 
-                        String inputLine;
-                        while ((inputLine = in.readLine()) != null) {
-                            sb.append(inputLine.replaceAll(" ", ""));
+                    String message = req.getParameter("name");
+
+                    List<City> cities = DBConnection.readDB();
+                    City foundedCity = null;
+                    for (City city : cities) {
+                        if (message.equalsIgnoreCase(city.getName())) {
+                            foundedCity = city;
+
+                            break;
                         }
-                    } catch (IOException e) {
-                        e.printStackTrace();
                     }
-                    pageVariables.put("lengthOfString", sb.toString().length());
-                    resp.getWriter().println(PageGenerator.instance().getPage("summString.html", pageVariables));
-        }
+
+                    if (foundedCity != null) {
+
+                        String url = foundedCity.getUrl();
+                        URL coldplace = new URL(url);
+                        StringBuilder sb = new StringBuilder();
+                        try (BufferedReader in = new BufferedReader(
+                                new InputStreamReader(coldplace.openStream()))) {
+
+                            String inputLine;
+                            while ((inputLine = in.readLine()) != null) {
+                                sb.append(inputLine.replaceAll(" ", ""));
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        pageVariables.put("lengthOfString", sb.toString().length());
+                        resp.getWriter().println(PageGenerator.instance().getPage("summString.html", pageVariables));
+                    }
+                }
         );
-}
+    }
+
 
     private static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
         Map<String, Object> pageVariables = new HashMap<>();
@@ -77,3 +86,4 @@ public class URLColdplaceServlet extends HttpServlet {
         return pageVariables;
     }
 }
+
