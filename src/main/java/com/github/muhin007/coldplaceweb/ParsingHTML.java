db428fd35a6e3b38337c4a6f9afa40e0b62693d7
Muhin007 <9226343456@mail.ru>
2018-06-19 16:56:21 +0500

    COLDWEB-9 Refactoring

--- src/main/java/com/github/muhin007/coldplaceweb/ParsingHTML.java
+++ src/main/java/com/github/muhin007/coldplaceweb/ParsingHTML.java
@@ -3,17 +3,19 @@ package com.github.muhin007.coldplaceweb;
 import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
+
 
 public class ParsingHTML {
     public static void main(String[] args) {
         Document doc = null;
         try {
-            doc = Jsoup.connect("https://rp5.ru/Погода_в_Челябинске").get();
+            doc = Jsoup.connect("https://www.gismeteo.ru/weather-chelyabinsk-4565").get();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String title = String.valueOf(doc.select("forecastShort-content"));
+        String title = String.valueOf(doc.select("div[class=value]"));
+
+
       System.out.println(title);
 
     }
