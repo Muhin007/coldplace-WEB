834910fcca801009908f428b1072abdcce8dde85
Mikhail Materukhin <9226343456@mail.ru>
2018-06-19 23:23:48 +0600

    COLDWEB-9 Используем JSoup для парсинга html (процесс разработки)

--- src/main/java/com/github/muhin007/coldplaceweb/ParsingHTML.java
+++ src/main/java/com/github/muhin007/coldplaceweb/ParsingHTML.java
@@ -1,6 +1,7 @@
 package com.github.muhin007.coldplaceweb;
 
 import java.io.IOException;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -9,13 +10,15 @@ public class ParsingHTML {
     public static void main(String[] args) {
         Document doc = null;
         try {
-            doc = Jsoup.connect("https://rp5.ru/Погода_в_Челябинске").get();
+            doc = Jsoup.connect("https://rp5.ru/Погода_в_Москве_(ВДНХ)").get();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String title = String.valueOf(doc.select("forecastShort-content"));
-      System.out.println(title);
+        Elements title = doc.select("div [id=forecastShort-content]");
+
+        System.out.println(title);
 
     }
+
 
 }
