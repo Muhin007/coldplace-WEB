6ab0ee96f7b750f00fe1fbde63b901b61048500c
Mikhail Materukhin <9226343456@mail.ru>
2018-08-06 23:24:29 +0500

    COLDWEB-12 Написание теста для SearchCityColdplaceServlet

--- src/main/java/com/github/muhin007/coldplaceweb/Servlets/URLColdplaceServlet.java
+++ src/main/java/com/github/muhin007/coldplaceweb/Servlets/URLColdplaceServlet.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.util.*;
 
 
+
 public class URLColdplaceServlet extends HttpServlet {
 
     public static String cityName;
---
+++ src/main/java/com/github/muhin007/coldplaceweb/Tests/SearchCityColdplaceServletTest.java
@@ -0,0 +1,36 @@
+package com.github.muhin007.coldplaceweb.Tests;
+
+import com.github.muhin007.coldplaceweb.Servlets.SearchCityColdplaceServlet;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import static javax.security.auth.callback.ConfirmationCallback.OK;
+import static javax.servlet.http.HttpServletResponse.*;
+import static org.junit.Assert.*;
+
+public class SearchCityColdplaceServletTest extends Mockito {
+
+
+    @Test
+    public void SearchCity() throws IOException {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+
+        when(request.getParameter("name")).thenReturn("Лондон");
+
+        assertEquals(true, response.getStatus(SC_OK));
+
+        new SearchCityColdplaceServlet().doPost(request, response);
+
+        verify(request, atLeast(1)).getParameter("name");
+        assertFalse(false, response.getStatus(SC_NOT_FOUND));
+    }
+
+}
