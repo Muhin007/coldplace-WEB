98f7e9303187a88761f67bf6d442a8cf60eb7329
Muhin007 <9226343456@mail.ru>
2018-08-27 12:28:24 +0500

    COLDWEB-12 тест DBService

--- src/main/test/com/github/muhin007/coldplaceweb/dbservice/DBServiceTest.java
+++ src/main/test/com/github/muhin007/coldplaceweb/dbservice/DBServiceTest.java
@@ -3,17 +3,14 @@ package com.github.muhin007.coldplaceweb.dbservice;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
 import static org.mockito.Mockito.mock;
 
 public class DBServiceTest {
-    Connection connection = mock(Connection.class);
+    DBService connection = mock(DBService.class);
 
     @Test
-    public void testConnection() throws SQLException {
-        connection.isClosed();
-        Assert.assertNotNull(connection.isClosed());
+    public void testConnection() {
+        connection.getConnection();
+        Assert.assertNotNull(connection.getConnection());
     }
 }
