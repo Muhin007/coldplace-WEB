a9a108ddb62dbcfd69e4d1b45825104ad35e0fc7
Mikhail_Materukhin <9226343456@mail.ru>
2018-10-01 15:46:09 +0500

    #2 Рефакторинг
    Обработка ошибок, логирование ошибок

--- src/main/java/com/github/muhin007/coldplaceweb/data/ReadDB.java
+++ src/main/java/com/github/muhin007/coldplaceweb/data/ReadDB.java
@@ -2,6 +2,7 @@ package com.github.muhin007.coldplaceweb.data;
 
 
 import com.github.muhin007.coldplaceweb.dbService.DBService;
+import org.apache.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -13,8 +14,9 @@ import java.util.List;
 import static com.github.muhin007.coldplaceweb.servlets.SignInServlet.*;
 
 public class ReadDB {
+    private static final Logger log = Logger.getLogger(ReadDB.class);
 
-    public static List<City> readCityFromDB() throws SQLException {
+    public static List<City> readCityFromDB() {
         String query = "select * from city";
         List<City> cities = new ArrayList<>();
         try (Connection con = DBService.getConnection();
@@ -29,11 +31,13 @@ public class ReadDB {
                 city.setURL(rs.getString("url"));
                 cities.add(city);
             }
+        } catch (SQLException e) {
+            log.error("Произошла ошибка. Подробности смотри в log-файле", e);
         }
         return cities;
     }
 
-    public static List<Temp> readTempFromDB() throws SQLException {
+    public static List<Temp> readTempFromDB() {
         String query = "select * from cityTemp";
         List<Temp> temps = new ArrayList<>();
         try (Connection con = DBService.getConnection();
@@ -47,11 +51,13 @@ public class ReadDB {
                 temp.setDate(rs.getString("date"));
                 temps.add(temp);
             }
+        } catch (SQLException e) {
+            log.error("Произошла ошибка. Подробности смотри в log-файле", e);
         }
         return temps;
     }
 
-    public  static List<UserProfile> readUserFromDB() throws  SQLException {
+    public  static List<UserProfile> readUserFromDB() {
         String query = "select * from users";
         List<UserProfile> userProfiles = new ArrayList<>();
         try (Connection con = DBService.getConnection();
@@ -67,6 +73,8 @@ public class ReadDB {
                 userProfile.setRole(rs.getString("role"));
                 userProfiles.add(userProfile);
             }
+        } catch (SQLException e) {
+            log.error("Произошла ошибка. Подробности смотри в log-файле", e);
         }
         return userProfiles;
     }
