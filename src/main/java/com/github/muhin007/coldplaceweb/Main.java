4dd8074e534d50e850a034f7a8f73080915f5214
Mikhail Materukhin <9226343456@mail.ru>
2018-10-15 23:00:25 +0500

    #18 LogOut

--- src/main/java/com/github/muhin007/coldplaceweb/Main.java
+++ src/main/java/com/github/muhin007/coldplaceweb/Main.java
@@ -9,7 +9,7 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String... args) throws Exception {
 
         freemarker.log.Logger.selectLoggerLibrary(freemarker.log.Logger.LIBRARY_NONE);
         org.apache.log4j.PropertyConfigurator.configure("log4j.properties");
@@ -21,9 +21,10 @@ public class Main {
         context.addServlet(new ServletHolder(new ButtonColdplaceServlet()), "/button");
         context.addServlet(new ServletHolder(new SearchCityColdplaceServlet()), "/search");
         context.addServlet(new ServletHolder(new URLColdplaceServlet()), "/URLRead");
+        context.addServlet(new ServletHolder(new LogOutServlet()), "/logout");
 
         ResourceHandler resource_handler = new ResourceHandler();
-        resource_handler.setResourceBase("templates");
+        resource_handler.setResourceBase(" ");
 
         HandlerList handlers = new HandlerList();
         handlers.setHandlers(new Handler[]{resource_handler, context});
@@ -35,6 +36,14 @@ public class Main {
         System.out.println("Сервер запущен http://localhost:8080/start");
         server.join();
     }
+
+    public static void x() throws Exception{
+        main();
+        main("ff");
+        main("ff", "asdf");
+        main("ff", "asdf", "asdfasw3r");
+    }
+
 }
 
 
